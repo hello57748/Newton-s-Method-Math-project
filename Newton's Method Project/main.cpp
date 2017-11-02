4a264711e9b189898478af2e71c5074c1d86da70
Sam Peters <hello57748@comcast.net>
2017-11-02 12:16:12 -0700

    Initial Commit

---
+++ Newton's Method Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Newton's Method Project
+//
+//  Created by Samuel Peters on 11/2/17.
+//  Copyright © 2017 Samuel Peters. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
